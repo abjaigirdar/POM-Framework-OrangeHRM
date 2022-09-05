cf88af3cd327a292cca1e43008fcc8de0c91bd25
abjaigirdar <abjaigirdar@gmail.com>
2022-09-04 23:15:43 -0400

    new

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,6 +3,7 @@ import browser.Browser;
 import browser.Info;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
@@ -16,6 +17,12 @@ public class BaseTest {
         driver.navigate().to(Info.URL);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.close();
+        driver.quit();
     }
 
     public LoginPage startLoginPage() {
